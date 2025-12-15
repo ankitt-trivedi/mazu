93a47cf81c2544122e781a502f37b54bbed8fff4
BIPL <neil.ankit@gmail.com>
2025-12-15 15:57:10 +0530

    add

--- src/test/java/listeners/ExtentListeners.java
+++ src/test/java/listeners/ExtentListeners.java
@@ -2,41 +2,27 @@ package listeners;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+
 import utils.ExtentManager;
+
 import org.testng.*;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class ExtentListeners implements ITestListener {
 
-	private static ExtentReports extent = ExtentManager.createInstance();
+    private static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
+    private static ExtentReports extent = ExtentManager.getInstance();
 
-	// Thread-safe map of tests
-	private static ConcurrentHashMap<Long, ExtentTest> testMap = new ConcurrentHashMap<>();
+    public static ExtentTest getTest() {
+        return test.get();
+    }
 
-	@SuppressWarnings("deprecation")
-	public static ExtentTest getTest() {
-		return testMap.get(Thread.currentThread().getId());
-	}
+    @Override
+    public void onTestStart(ITestResult result) {
+        test.set(extent.createTest(result.getMethod().getMethodName()));
+    }
 
-	@SuppressWarnings("deprecation")
-	@Override
-	public void onTestStart(ITestResult result) {
-		ExtentTest test = extent.createTest(result.getMethod().getMethodName());
-		testMap.put(Thread.currentThread().getId(), test);
-	}
-
-	@Override
-	public void onTestSuccess(ITestResult result) {
-		getTest().pass("Test passed");
-	}
-
-	@Override
-	public void onTestFailure(ITestResult result) {
-		getTest().fail(result.getThrowable());
-	}
-
-	@Override
-	public void onFinish(ITestContext context) {
-		extent.flush();
-	}
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush();
+    }
 }
--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -5,16 +5,20 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
 public class ExtentManager {
 
-	private static ExtentReports extent;
+    private static ExtentReports extent;
 
-	public static ExtentReports createInstance() {
-		if (extent == null) {
-			String reportPath = System.getProperty("user.dir") + "/reports/ExtentReport.html";
-			ExtentSparkReporter reporter = new ExtentSparkReporter(reportPath);
+    public static ExtentReports getInstance() {
 
-			extent = new ExtentReports();
-			extent.attachReporter(reporter);
-		}
-		return extent;
-	}
+        if (extent == null) {
+            ExtentSparkReporter spark =
+                    new ExtentSparkReporter("reports/index.html");
+
+            spark.config().setReportName("Mazu Automation Report");
+            spark.config().setDocumentTitle("Test Execution Report");
+
+            extent = new ExtentReports();
+            extent.attachReporter(spark);
+        }
+        return extent;
+    }
 }
