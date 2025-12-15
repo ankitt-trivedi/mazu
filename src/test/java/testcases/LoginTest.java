39cb57c574f687fd6252cd3c9a568b56f2fb9a7a
BIPL <neil.ankit@gmail.com>
2025-12-15 17:15:35 +0530

    add

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -25,7 +25,7 @@ public class LoginTest extends BaseTest {
 		type("password","Ankit@123");
 		click("Login2");
 		click("createinvoice");
-		type("invoicenumber","invoice17");
+		type("invoicenumber","invoice18");
 		click("billto");
 		click("billtooption");
 		click("itemname");
